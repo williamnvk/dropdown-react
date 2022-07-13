2a366de5c707718b388cd84d1272a3f67c23ca01
William Novak <williamnvk@gmail.com>
2022-07-12 23:47:49 -0300

    fix: minor fixed

--- src/app.tsx
+++ src/app.tsx
@@ -8,6 +8,7 @@ import cn from "classnames";
 export default function App() {
   const [openOne, setOpenOne] = useState(false);
   const [openTwo, setOpenTwo] = useState(false);
+  const [openThree, setOpenThree] = useState(false);
 
   const toggleOne = () => {
     setOpenOne(!openOne);
@@ -17,16 +18,34 @@ export default function App() {
     setOpenTwo(!openTwo);
   };
 
+  const toggleThree = () => {
+    setOpenThree(!openThree);
+  };
+
+  const optionsAsArray = [
+    {
+      id: "i-loved-this",
+      label: "Like",
+      onClick: () => alert("liked")
+    },
+    {
+      id: "i-hate-this",
+      label: "Dislike",
+      onClick: () => alert("disliked")
+    }
+  ];
+
   return (
     <div className={cn(s.root)}>
       {/** Eg.: 1
        * renderiza o menu de itens a esquerda
        */}
       <Dropdown
-        key="eg-1"
+        key="eg-one"
         open={openOne}
         toggle={toggleOne}
         icon={<MoreIcon />}
+        tabIndex={0}
       >
         <DropdownItem href="/">Rename</DropdownItem>
         <DropdownItem onClick={() => alert(`delete pressed`)}>
@@ -41,10 +60,11 @@ export default function App() {
        * renderiza o menu de itens a esquerda
        */}
       <Dropdown
-        key="eg-2"
+        key="eg-two"
         open={openTwo}
         toggle={toggleTwo}
         icon={<MoreIcon />}
+        tabIndex={-1}
       >
         <DropdownItem href="/">Rename</DropdownItem>
         <DropdownItem onClick={() => alert(`delete pressed`)}>
@@ -53,6 +73,23 @@ export default function App() {
         <DropdownItem href="/" onClick={(e) => e.preventDefault()}>
           Share
         </DropdownItem>
+      </Dropdown>
+
+      {/** Eg.: 3
+       * alimentando as options com array
+       */}
+      <Dropdown
+        key="eg-three"
+        open={openThree}
+        toggle={toggleThree}
+        icon={<MoreIcon />}
+        tabIndex={-1}
+      >
+        {optionsAsArray.map((option) => (
+          <DropdownItem key={option.id} onClick={option.onClick}>
+            {option.label}
+          </DropdownItem>
+        ))}
       </Dropdown>
     </div>
   );
